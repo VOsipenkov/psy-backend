cdaf03263372c718df153af53500f9d728b96f17
VOsipenkov <jumpViaOcean@gmail.com>
2025-11-11 01:12:43 +0300

    init repo

---
+++ src/test/java/psy/back/psy/PsyApplicationTests.java
@@ -0,0 +1,13 @@
+package psy.back.psy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
