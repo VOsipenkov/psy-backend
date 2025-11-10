d78432310fe50975bf1f4bdbf6c1b7f3669ba5bb
VOsipenkov <jumpViaOcean@gmail.com>
2025-11-11 02:13:36 +0300

    fix Message dto

--- src/main/java/psy/back/messages/Message.java
+++ src/main/java/psy/back/messages/Message.java
@@ -1,10 +1,8 @@
 package psy.back.messages;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class Message {
-    @NotNull(message = "Text must not be null")
     private String text;
 }
