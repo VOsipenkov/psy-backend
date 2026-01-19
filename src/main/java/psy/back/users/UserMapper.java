0d8a1c5320f27d2bc4121f8e0e8b3ed16e06030d
VOsipenkov <jumpViaOcean@gmail.com>
2026-01-20 01:47:07 +0300

    one more fix

--- src/main/java/psy/back/users/UserMapper.java
+++ src/main/java/psy/back/users/UserMapper.java
@@ -3,6 +3,7 @@ package psy.back.users;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 
 import java.nio.charset.StandardCharsets;
@@ -11,10 +12,11 @@ import java.nio.charset.StandardCharsets;
 public interface UserMapper {
 
     @Mapping(target = "login", source = "login")
-    @Mapping(target = "password", expression = "java(encodeToBase64(user.getPassword()))")
+    @Mapping(target = "password", source = "password", qualifiedByName = "toBase64")
     UserEntity toEntity(User user);
 
-    default String toBase64(String text){
-        return Base64.encodeBase64String(text.getBytes(StandardCharsets.UTF_8));
+    @Named("toBase64")
+    default String toBase64(String text) {
+        return java.util.Base64.getEncoder().encodeToString(text.getBytes(StandardCharsets.UTF_8));
     }
 }
