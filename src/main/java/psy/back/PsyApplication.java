27af0c692e37ef08b69a2ebb67d8aa948cbf660b
VOsipenkov <jumpViaOcean@gmail.com>
2026-03-10 00:19:38 +0300

    connect with qwen3:4b model

--- src/main/java/psy/back/PsyApplication.java
+++ src/main/java/psy/back/PsyApplication.java
@@ -1,13 +1,24 @@
 package psy.back;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
 @SpringBootApplication
 public class PsyApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(PsyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(PsyApplication.class, args);
+        ChatClient chatClient = (ChatClient)context.getBean("chatClient");
+        var response = chatClient.prompt().user("Какая ты версия модели?").call().content();
+        System.out.println("Model version response" + response);
+    }
+
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder builder) {
+        return builder.build();
+    }
 }
